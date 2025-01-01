6ba7f4a4133421c904dd5ac6793f2f5235c8f8b0
nafri-dev <mdirfantreasurer@gmail.com>
2025-01-01 14:49:11 +0530

    Final commit

--- src/section/home/Testimonial Section/TestimonialSection.jsx
+++ src/section/home/Testimonial Section/TestimonialSection.jsx
@@ -1,6 +1,8 @@
 import styles from "@/section/home/Testimonial Section/TestimonialSection.module.css";
 import image1 from "@/assets/home/Testimony.png";
 import image2 from "@/assets/home/Testimony2.png";
+import stu3 from "@/assets/home/Testimony 3.png";
+import image4 from "@/assets/home/Testimony 4.png";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
@@ -11,7 +13,7 @@ const TestimonialSection = () => {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768); // Adjust the breakpoint as needed
+      setIsMobile(window.innerWidth <= 500); // Adjust the breakpoint as needed
     };
 
     // Initial check
@@ -35,9 +37,9 @@ const TestimonialSection = () => {
         <div className={styles.grid}>
           {/* Testimonial Card 1 */}
           <div className={styles.card}>
-            {isMobile ?    <Image src={image1.src} alt="img"  layout="responsive"
+            {isMobile ?    <Image src={image4.src} alt="img"  layout="responsive"
                              width={100}
-                             height={100}/> :    <Image src={image2.src} alt="img"  layout="responsive"
+                             height={100}/> :    <Image src={image1.src} alt="img"  layout="responsive"
                              width={100}
                              height={100}/>}
            
@@ -46,7 +48,7 @@ const TestimonialSection = () => {
           {/* Testimonial Card 2 */}
           <div className={styles.card}>
           <div className={styles.card}>
-            {isMobile ?    <Image src={image1.src} alt="img"  layout="responsive"
+            {isMobile ?    <Image src={stu3.src} alt="img"  layout="responsive"
                              width={100}
                              height={100}/> :    <Image src={image2.src} alt="img"  layout="responsive"
                              width={100}
